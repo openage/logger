460dfe4739b7e0a93fd532b31b7a7ea86e7bb07f
Sunny Parkash <sunny.parkash@gmail.com>
2018-04-02 00:57:08 +0530

    adding context tree and stats logger

--- index.js
+++ index.js
@@ -1,92 +1,114 @@
-'use strict';
+'use strict'
 
-var winston = require('winston');
-var logConfig = require('config').get('logger');
+var winston = require('winston')
+var logConfig = require('config').get('logger')
 
-var transports = [];
+var transports = []
 
 if (logConfig.file) {
-    transports.push(new winston.transports.File(logConfig.file));
+    transports.push(new winston.transports.File(logConfig.file))
 }
 
 if (logConfig.console) {
-    transports.push(new winston.transports.Console(logConfig.console));
+    transports.push(new winston.transports.Console(logConfig.console))
 }
 
 if (logConfig.http) {
-    transports.push(new winston.transports.Http(logConfig.http));
+    transports.push(new winston.transports.Http(logConfig.http))
 }
 
 var defaultLogger = new winston.Logger({
     transports: transports,
     exitOnError: false
-});
+})
 
 defaultLogger.stream = {
-    write: function(message, encoding) {
-        defaultLogger.info(message);
+    write: function (message, encoding) {
+        defaultLogger.info(message)
     }
-};
+}
 
-
-module.exports = function(ctx) {
-
-    var logger = new winston.Logger({
+module.exports = function (ctx) {
+    var winstonLogger = new winston.Logger({
         transports: transports,
         exitOnError: false
-    });
+    })
 
-    var stringifiedCtx = function(param) {
+    var stringifiedCtx = function (param) {
         if (ctx) {
-            return '[' + ctx + (param ? ':' + param : '') + '] ';
+            return '[' + ctx + (param ? ':' + param : '') + '] '
         } else if (param) {
-            return '[' + param + '] ';
+            return '[' + param + '] '
         } else {
-            return '';
+            return ''
         }
-    };
+    }
 
-    var insertCtx = function(params, additional) {
-        if (typeof params[0] === 'string') {
-            params[0] = stringifiedCtx(additional) + params[0];
-        } else if (typeof params[0] === 'object') {
-            Array.prototype.unshift.call(params, stringifiedCtx(additional));
+    var insertCtx = function (params, additional) {
+        if (typeof params[ 0 ] === 'string') {
+            params[ 0 ] = stringifiedCtx(additional) + params[ 0 ]
+        } else if (typeof params[ 0 ] === 'object') {
+            Array.prototype.unshift.call(params, stringifiedCtx(additional))
         }
 
-        return params;
-    };
+        return params
+    }
 
-    var decorator = function(param) {
+    var loggerFactory = function (newCtx, parent) {
+        let subContext = (parent && parent.subContext) ? `${parent.subContext}:${newCtx}` : newCtx
+
         return {
-            error: function() {
-                logger.error.apply(this, insertCtx(arguments, param));
+            subContext: subContext,
+            parent: parent,
+            fatal: function () {
+                // TODO: send alert
+                winstonLogger.error.apply(this, insertCtx(arguments, subContext))
             },
-            warn: function() {
-                logger.warn.apply(this, insertCtx(arguments, param));
+            error: function () {
+                winstonLogger.error.apply(this, insertCtx(arguments, subContext))
             },
-            info: function() {
-                logger.info.apply(this, insertCtx(arguments, param));
+            warn: function () {
+                winstonLogger.warn.apply(this, insertCtx(arguments, subContext))
             },
-            verbose: function() {
-                logger.varbose.apply(this, insertCtx(arguments, param));
+            info: function () {
+                winstonLogger.info.apply(this, insertCtx(arguments, subContext))
             },
-            debug: function() {
-                logger.debug.apply(this, insertCtx(arguments, param));
+            verbose: function () {
+                winstonLogger.verbose.apply(this, insertCtx(arguments, subContext))
             },
-            silly: function() {
-                logger.silly.apply(this, insertCtx(arguments, param));
+            debug: function () {
+                winstonLogger.debug.apply(this, insertCtx(arguments, subContext))
+            },
+            silly: function () {
+                winstonLogger.silly.apply(this, insertCtx(arguments, subContext))
+            },
+            end: function () {
+                if (this.startTime) {
+                    let span = (new Date() - this.startTime) / 1000
+
+                    winstonLogger.info.apply(this, insertCtx([ `took: ${span} second(s)` ], subContext))
+                }
             }
-        };
-    };
+        }
+    }
 
-    var decoratorObj = decorator();
+    var start = function (param, parent) {
+        var newLogger = loggerFactory(param, parent)
+        newLogger.startTime = new Date()
+        newLogger.start = function (param) {
+            return start(param, newLogger)
+        }
 
-    decoratorObj.start = function(param) {
-        var wrapper = decorator(param);
+        newLogger.debug('started')
 
-        wrapper.debug('started');
-        return wrapper;
-    };
+        return newLogger
+    }
 
-    return decoratorObj;
-};
+    var logger = loggerFactory()
+
+    logger.start = function (param) {
+        return start(param, logger)
+    }
+
+    return logger
+}
